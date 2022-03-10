07c047de4c92c05fd85524f54ef56e0bd740031b
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2022-03-11 00:24:54 +0530

    add: cart page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Navbar } from "./components";
-import { Home, Products, Wishlist } from "../pages/pages";
+import { Cart, Home, Products, Wishlist } from "../pages/pages";
 
 export const Main = () => {
   const [route, setRoute] = useState("/");
@@ -11,6 +11,7 @@ export const Main = () => {
       {route === "/" && <Home />}
       {route === "/products" && <Products />}
       {route === "/wishlist" && <Wishlist />}
+      {route === "/cart" && <Cart />}
     </div>
   );
 };
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,7 @@
+export const Cart = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-center m-sm-tb">Cart</h1>
+    </div>
+  );
+};
