c95f620d7d63900507325198ebb99fad00f27b50
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2022-03-11 09:37:42 +0530

    refactor: add separate file for cart-reducer

--- src/contexts/cart-context.js
+++ src/contexts/cart-context.js
@@ -1,34 +1,7 @@
+import { cartReducer } from "../reducers/cart-reducer";
 const { createContext, useReducer, useContext } = require("react");
 
 const initialState = { cartItems: {} };
-const cartReducer = (state, { type, payload: { id, name, price, img } }) => {
-  switch (type) {
-    case "ADD_TO_CART":
-      return state.cartItems[id]
-        ? {
-            ...state,
-            cartItems: {
-              ...state.cartItems,
-              [id]: { ...state.cartItems[id], qty: state.cartItems[id].qty + 1 }
-            }
-          }
-        : {
-            ...state,
-            cartItems: {
-              ...state.cartItems,
-              [id]: { id, name, price, img, qty: 1 }
-            }
-          };
-
-    case "REMOVE_FROM_CART":
-      const cartItems = { ...state.cartItems };
-      delete cartItems[id];
-      return { ...state, cartItems };
-
-    default:
-      return state;
-  }
-};
 
 const CartContext = createContext(null);
 
---
+++ src/reducers/cart-reducer.js
@@ -0,0 +1,31 @@
+export const cartReducer = (
+  state,
+  { type, payload: { id, name, price, img } }
+) => {
+  switch (type) {
+    case "ADD_TO_CART":
+      return state.cartItems[id]
+        ? {
+            ...state,
+            cartItems: {
+              ...state.cartItems,
+              [id]: { ...state.cartItems[id], qty: state.cartItems[id].qty + 1 }
+            }
+          }
+        : {
+            ...state,
+            cartItems: {
+              ...state.cartItems,
+              [id]: { id, name, price, img, qty: 1 }
+            }
+          };
+
+    case "REMOVE_FROM_CART":
+      const cartItems = { ...state.cartItems };
+      delete cartItems[id];
+      return { ...state, cartItems };
+
+    default:
+      return state;
+  }
+};
