1e5a99bbe5d8e23a56f8b629a58d059ca43c9c56
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2022-03-11 16:15:55 +0530

    fix: changed class keyword to className

--- src/components/CartInfo.jsx
+++ src/components/CartInfo.jsx
@@ -8,25 +8,25 @@ export const CartInfo = ({ cartItems }) => {
   const totalAmount = totalCartPrice + deliveryCharge - discount;
   return (
     <div>
-      <div class="cart-detail-card">
-        <h2 class="text-base  m-sm-tb ">Price Details</h2>
-        <div class="m-sm-tb">
+      <div className="cart-detail-card">
+        <h2 className="text-base  m-sm-tb ">Price Details</h2>
+        <div className="m-sm-tb">
           <span>Price ({getNumberOfItemsInCart(cartItems)} item)</span>
           <span>${totalCartPrice} USD</span>
         </div>
-        <div class="m-sm-tb">
+        <div className="m-sm-tb">
           <span>Discount</span>
           <span>- ${discount} USD</span>
         </div>
-        <div class="m-sm-tb">
+        <div className="m-sm-tb">
           <span>Delivery Charges</span>
           <span>$20.00 USD</span>
         </div>
-        <h2 class="text-base m-sm-tb">
+        <h2 className="text-base m-sm-tb">
           <span>Total Amount</span>
           <span>${totalAmount} USD</span>
         </h2>
-        <button class="btn btn-primary m-xs-tb">PLACE ORDER</button>
+        <button className="btn btn-primary m-xs-tb">PLACE ORDER</button>
       </div>
     </div>
   );
